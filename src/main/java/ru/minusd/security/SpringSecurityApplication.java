2add54725725c7d6f8d8aff979efb769e832b896
AVAndrianov <a.v.andrianov33@gmail.com>
2025-04-28 22:20:58 +0300

    123

--- src/main/java/ru/minusd/security/SpringSecurityApplication.java
+++ src/main/java/ru/minusd/security/SpringSecurityApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories({"com.mycompany.myproject"})
-//@EntityScan({"com.mycompany.myproject"})
 @SpringBootApplication
 @ComponentScan(basePackages = "ru.minusd")
 public class SpringSecurityApplication {
